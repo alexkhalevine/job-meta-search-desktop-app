a267c69e20fd8896276b3077db162de919f0df74
alexandre khalevine <alexandre.khalevine@apryse.com>
2025-07-27 18:56:00 +0200

    fixed type erorrs during build time

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,35 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
 
+interface CustomElectronAPI {
+  platform: string
+  searchJobs: (config: SearchConfig) => Promise<{
+    success: boolean
+    data?: JobPost[]
+    error?: string
+    meta: { discardedCount: number }
+  }>
+  getJobSources: () => Promise<string[]>
+}
+
+interface SearchConfig {
+  searchQuery: string
+  location: string
+}
+
+interface JobPost {
+  title: string
+  company: string
+  location: string
+  remote: boolean
+  description: string
+  url: string
+  source: string
+}
+
 declare global {
   interface Window {
     electron: ElectronAPI
+    electronAPI: CustomElectronAPI
     api: unknown
   }
 }
