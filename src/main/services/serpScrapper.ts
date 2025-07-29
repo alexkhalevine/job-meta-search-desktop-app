d8b8356843803bebefa6798dbec3b03ed955570a
alexandre khalevine <alexandre.khalevine@apryse.com>
2025-07-29 20:19:37 +0200

    added working google jobs

--- src/main/services/serpScrapper.ts
+++ src/main/services/serpScrapper.ts
@@ -4,6 +4,22 @@ import { SettingsLoader } from '@utils/settingsLoader'
 
 const SERPAPI_URL = 'https://serpapi.com/search.json'
 
+type GoogleJobsType = {
+  title: string
+  company_name: string
+  location: string
+  via: string
+  share_link: string
+  thumbnail: string
+  extensions: string[]
+  detected_extensions: {
+    posted_at: string
+    schedule_type: string
+  }
+  description: string
+  job_id: string
+}
+
 export async function serpScrapper(config: SearchConfig): Promise<JobPost[]> {
   const SERPAPI_KEY = SettingsLoader.getSerpApiKey()
 
@@ -12,36 +28,40 @@ export async function serpScrapper(config: SearchConfig): Promise<JobPost[]> {
     return []
   }
 
-  const response = await axios.get(SERPAPI_URL, {
-    params: {
-      engine: 'google_jobs',
-      q: config.searchQuery,
-      location: config.location,
-      api_key: SERPAPI_KEY
-    }
-  })
-
-  const results = response.data.jobs_results
-  if (!Array.isArray(results)) return []
-
-  console.log('google jobs: ', results)
-
-  const jobs: JobPost[] = results.map(
-    (job: any): JobPost => ({
-      title: job.title || '',
-      company: job.company || '',
-      location: job.location || '',
-      remote: /remote|home\s*office/i.test((job.location ?? '') + (job.description ?? '')),
-      description:
-        job.description +
-        ' More info: ' +
-        Object.entries(job.detected_extensions)
-          .map(([key, value]) => `${key}: ${value}`)
-          .join(', '),
-      url: job.apply_options.map((opt: any) => `${opt.title}: ${opt.link}`),
-      source: 'google jobs'
+  try {
+    const response = await axios.get(SERPAPI_URL, {
+      params: {
+        engine: 'google_jobs',
+        q: config.searchQuery,
+        location: config.location,
+        api_key: SERPAPI_KEY
+      }
     })
-  )
 
-  return jobs
+    const results = response.data.jobs_results
+    if (!Array.isArray(results)) return []
+
+    console.log('google jobs: ', results)
+
+    const jobs: JobPost[] = results.map(
+      (job: GoogleJobsType): JobPost => ({
+        title: job.title || '',
+        company: job.company_name || '',
+        location: job.location || '',
+        remote: /remote|home\s*office/i.test((job.location ?? '') + (job.description ?? '')),
+        description:
+          job.description +
+          ' More info: ' +
+          Object.entries(job.detected_extensions)
+            .map(([key, value]) => `${key}: ${value}`)
+            .join(', '),
+        url: job.share_link,
+        source: `google jobs - ${job.via}`
+      })
+    )
+    return jobs
+  } catch (err) {
+    console.error('Error scraping google jobs:', err)
+    throw new Error(`Failed to scrape google jobs: ${err}`)
+  }
 }
